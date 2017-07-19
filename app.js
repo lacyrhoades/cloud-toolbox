8e028ab8ef2319ab1ed71d7d94fb8c791e1044f7
Lacy Rhoades <lacyrhoades@gmail.com>
2017-07-18 21:24:30 -0400

    Removing debug code, added engines config

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-console.log(path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
